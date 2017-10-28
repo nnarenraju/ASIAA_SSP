ba21c62d998467fb291065586d0b54290ae2c84b
N. Naren Raju <nnarenraju@gmail.com>
2017-10-28 12:14:55 +0000

    Update HSC_convolve.py

--- HSC_convolve.py
+++ HSC_convolve.py
@@ -11,13 +11,13 @@ Created on Mon Oct 16 03:27:52 2017
 __author__      = "nnarenraju"
 __copyright__   = "Copyright 2017, AGN_Classification"
 __credits__     = "nnarenraju"
-__license__     = "GPL"
+__license__     = "Apache License 2.0"
 __version__     = "1.0.1"
 __maintainer__  = "nnarenraju"
 __email__       = "nnarenraju@gmail.com"
 __status__      = "inUsage"
 
-Github Repository: "Add link here"
+Github Repository: "https://github.com/nnarenraju/ASIAA_SSP"
 
 """
 
